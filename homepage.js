8f49937c997f9f0e48f19e07e23529ccdc3bbd1a
1049404 <143446483+1049404@users.noreply.github.com>
2024-01-30 12:35:51 -0800

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,6 @@
+let SFTWR_DEV_PAGE = document.querySelector("#compsci");
+
+SFTWR_DEV_PAGE.addEventListener("click", GoToSoftwarePage);
+function GoToSoftwarePage(){
+    window.location.href = "secondpage.html";
+}
