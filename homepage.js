d2f7a95565bb3d888fb83836e24e5fea55b7b8b0
1049404 <143446483+1049404@users.noreply.github.com>
2024-01-30 12:38:02 -0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,5 +2,5 @@ let SFTWR_DEV_PAGE = document.querySelector("#compsci");
 
 SFTWR_DEV_PAGE.addEventListener("click", GoToSoftwarePage);
 function GoToSoftwarePage(){
-    window.location.href = "Software_And_Systems_Development.htmll";
+    window.location.href = "Software_And_Systems_Development.html";
 }
